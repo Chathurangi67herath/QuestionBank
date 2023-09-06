7cb79dfbd9671cf91c42d5c1f34e2b952e8fe439
Chathurangi67herath <73054107+Chathurangi67herath@users.noreply.github.com>
2023-09-06 13:30:22 +0530

    fouth screen complete

---
+++ fourth.js
@@ -0,0 +1,55 @@
+document.getElementById("submit").onclick = function(){
+    var x = document.getElementById("symbol").value;
+    var y = document.getElementById("variable").value;
+    var z = document.getElementById("percentage").value;
+    document.getElementById("we").style.visibility="visible";
+
+    if ((x=="&A") && (y =='sum') && (z =="%d")){
+        document.getElementById("we").style.backgroundColor="rgb(32, 10, 94)";
+        document.getElementById("we").innerHTML="Well Done !";
+       document.getElementById("next").style.visibility="visible";
+       document.getElementById("submit").style.display="none";
+       document.getElementById("try").style.display="none";
+       document.getElementById("symbol").style.backgroundColor="#9CA9FB";
+       document.getElementById("variable").style.backgroundColor="#9CA9FB";
+       document.getElementById("percentage").style.backgroundColor="#9CA9FB";
+        confetti.start();
+    }
+    else{
+        
+        document.getElementById("we").style.backgroundColor="red";
+        document.getElementById("we").innerHTML="Incorrect. Try Again !";
+        document.getElementById("try").style.backgroundColor="#FABFCA";
+        document.getElementById("try").style.visibility="visible";
+
+       
+       if(x!="&A") {
+        document.getElementById("symbol").style.backgroundColor="#FABFCA";
+       }
+       if(y!='sum') {
+        document.getElementById("variable").style.backgroundColor="#FABFCA";
+       }
+       if(z!="%d") {
+        document.getElementById("percentage").style.backgroundColor="#FABFCA";
+       }
+    
+
+
+    }
+}
+
+document.getElementById("try").onclick = function(){
+    document.getElementById("symbol").style.backgroundColor="";
+    document.getElementById("symbol").value="";
+    document.getElementById("variable").style.backgroundColor="";
+    document.getElementById("variable").value="";
+    document.getElementById("percentage").style.backgroundColor="";
+    document.getElementById("percentage").value="";
+    document.getElementById("we").style.backgroundColor="blue";
+    document.getElementById("we").innerHTML="You Can. Try Again !";
+    document.getElementById("try").style.backgroundColor="lightblue";
+
+    
+    
+    
+}
